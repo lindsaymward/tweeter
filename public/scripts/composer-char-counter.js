d3d6b272f5ebcb8260b410af2509c513ea27114f
Lindsay Ward <lward23@hotmail.com>
2023-05-22 17:56:17 -0700

    Implement changing character count with jQuery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  $('#tweet-text').on('input', function(event) {
+    const length = $(this).val().length;
+    const counter = $(this).next().children(".counter");
+    const charLeft = counter.val(140 - length);
+    counter.toggleClass("red", charLeft.val() < 0);
+  });
+});
