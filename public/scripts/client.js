4f9a01e5ee6589cd683cc8ae662eb8c2dcb39b6a
Lindsay Ward <lward23@hotmail.com>
2023-05-24 09:33:44 -0700

    Add ajax get request to load tweets from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,33 +38,13 @@ $('document').ready(function() {
     event.preventDefault();
     $.post("/tweets", $(this).serialize());
   });
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  renderTweets(data);
+
+  const loadTweets = function() {
+    $.get('/tweets', null, function(data) {
+      renderTweets(data);
+    }, 'json');
+  }
+
+  loadTweets();
 });
 
